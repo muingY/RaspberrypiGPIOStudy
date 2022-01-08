cd3a96643134ab0fc7228a2e2a61a7ba28f80c43
muing <ungetqqq@gmail.com>
2022-01-08 11:28:12 +0900

    Control servo

---
+++ ServoMotor/ControlServo.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <wiringPi.h>
+#include <softPwm.h>
+
+#define PIN_SERVO 0
+
+int main()
+{
+    std::cout << "<GPIO Study 03>" << std::endl << ": Control servo motor with pwm." << std::endl;
+
+    if (wiringPiSetup() == -1)
+    {
+        std::cout << "ERROR! wiringPiSetup fail." << std::endl;
+        return -1;
+    }
+
+    softPwmCreate(PIN_SERVO, 0, 200);
+
+    while (true)
+    {
+        softPwmWrite(PIN_SERVO, 15);
+        delay(1000);
+        softPwmWrite(PIN_SERVO, 24);
+        delay(1000);
+        softPwmWrite(PIN_SERVO, 5);
+        delay(1000);
+    }
+
+    return 0;
+}
